402d84636765a0bf7af0575ab00a2cd0e88db063
massimorid <mridella.ieu2022@student.ie.edu>
2025-02-23 20:54:39 +0100

    pupil detection

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cv2
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-
+#oh damn
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
